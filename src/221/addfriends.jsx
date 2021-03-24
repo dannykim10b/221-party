40d694d1a7e2d61b58af466f8196437e4760734a
Danny <danny.kim1097@gmail.com>
2021-03-24 17:00:44 -0500

    refactor addfriends, update images

--- src/221/addfriends.jsx
+++ src/221/addfriends.jsx
@@ -5,33 +5,11 @@ import FormControl from '@material-ui/core/FormControl';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import "./221.css";
-import danny from '../img/dannycrop.jpg';
-import gabe from '../img/gabecrop.jpg';
-import john from '../img/Johncrop.jpg';
-import kaiden from '../img/Kaidencrop.jpg';
-import justin from '../img/justincorp.jpg';
-import susan from '../img/Susancrop.jpg';
-import kayoung from '../img/kayoungcrop.jpg';
-import joice from '../img/joicecrop.jpg';
-import hannah from '../img/Hannahcrop.jpg';
-import annie from '../img/anniecrop.jpg'
 
-export default function AddFriends() {
-  const [state, setState] = useState({
-      danny: false,
-      gabe: false,
-      john: false,
-      kaiden: false,
-      justin: false,
-      susan: false,
-      kayoung: false,
-      joice: false,
-      hannah: false,
-      annie: false,
-  });
+export default function AddFriends(props) {
 
   const handleChange = (e) => {
-    setState({...state, [e.target.name]: e.target.checked});
+    props.toggleFriends(e);
   };
 
   return (
@@ -42,62 +20,50 @@ export default function AddFriends() {
       <FormGroup row>
       <FormGroup>
         <FormControlLabel
-          control={<Switch size="small" checked={state.danny} onChange={handleChange} name="danny" />}
+          control={<Switch size="small" onChange={handleChange} name="danny" />}
           label="Danny"
         />
         <FormControlLabel
-          control={<Switch size="small" checked={state.gabe} onChange={handleChange} name="gabe" />}
+          control={<Switch size="small" onChange={handleChange} name="gabe" />}
           label="Gabe"
         />
         <FormControlLabel
-          control={<Switch size="small" checked={state.john} onChange={handleChange} name="john" />}
+          control={<Switch size="small" onChange={handleChange} name="john" />}
           label="John"
         />
         <FormControlLabel
-          control={<Switch size="small" checked={state.kaiden} onChange={handleChange} name="kaiden" />}
+          control={<Switch size="small" onChange={handleChange} name="kaiden" />}
           label="Kaiden"
         />
         <FormControlLabel
-          control={<Switch size="small" checked={state.justin} onChange={handleChange} name="justin" />}
+          control={<Switch size="small" onChange={handleChange} name="justin" />}
           label="Justin"
         />
       </FormGroup>
       <FormGroup>
         <FormControlLabel
-          control={<Switch size="small" checked={state.susan} onChange={handleChange} name="susan" />}
+          control={<Switch size="small" onChange={handleChange} name="susan" />}
           label="Susan"
         />
         <FormControlLabel
-          control={<Switch size="small" checked={state.kayoung} onChange={handleChange} name="kayoung" />}
+          control={<Switch size="small" onChange={handleChange} name="kayoung" />}
           label="Kayoung"
         />
         <FormControlLabel
-          control={<Switch size="small" checked={state.joice} onChange={handleChange} name="joice" />}
+          control={<Switch size="small" onChange={handleChange} name="joice" />}
           label="Joice"
         />
         <FormControlLabel
-          control={<Switch size="small" checked={state.hannah} onChange={handleChange} name="hannah" />}
+          control={<Switch size="small" onChange={handleChange} name="hannah" />}
           label="Hannah"
         />
         <FormControlLabel
-          control={<Switch size="small" checked={state.annie} onChange={handleChange} name="annie" />}
+          control={<Switch size="small" onChange={handleChange} name="annie" />}
           label="Annie"
         />
       </FormGroup>
       </FormGroup>
     </FormControl>
-    </div>
-    <div>
-      {state.danny && <img class="friend danny" src={danny} width="75" height="75" alt="danny"/>}
-      {state.gabe && <img class="friend gabe" src={gabe} width="75" height="75" alt="gabe"/>}
-      {state.john && <img class="friend john" src={john} width="75" height="75" alt="john"/>}
-      {state.kaiden && <img class="friend kaiden" src={kaiden} width="75" height="75" alt="kaiden"/>}
-      {state.justin && <img class="friend justin" src={justin} width="75" height="75" alt="justin"/>}
-      {state.susan && <img class="friend susan" src={susan} width="75" height="75" alt="susan"/>}
-      {state.kayoung && <img class="friend kayoung" src={kayoung} width="75" height="75" alt="kayoung"/>}
-      {state.joice && <img class="friend joice" src={joice} width="75" height="75" alt="joice"/>}
-      {state.hannah && <img class="friend hannah" src={hannah} width="75" height="75" alt="hannah"/>}
-      {state.annie && <img class="friend annie" src={annie} width="75" height="75" alt="annie"/>}
     </div>
     </>
   );
--- src/221/inside221.jsx
+++ src/221/inside221.jsx
@@ -4,9 +4,35 @@ import AddFriends from './addfriends'
 import Darkmode from './darkmode'
 import room from '../img/221testbackgroundcropped.png'
 import "./221.css"
+import danny from '../img/dannycrop.png';
+import gabe from '../img/gabecrop.png';
+import john from '../img/johncrop.png';
+import kaiden from '../img/kaidencrop.png';
+import justin from '../img/justincrop.png';
+import susan from '../img/susancrop.png';
+import kayoung from '../img/kayoungcrop.png';
+import joice from '../img/joicecrop.png';
+import hannah from '../img/hannahcrop.png';
+import annie from '../img/anniecrop.png'
 
 export default function Inside221(props) {
     const [darkmode, setDarkmode] = useState(false)
+    const [state, setState] = useState({
+        danny: false,
+        gabe: false,
+        john: false,
+        kaiden: false,
+        justin: false,
+        susan: false,
+        kayoung: false,
+        joice: false,
+        hannah: false,
+        annie: false,
+    });
+
+    const toggleFriends = (e) => {
+        setState({...state, [e.target.name]: e.target.checked});
+      };
 
     const toggleDarkmode = () => {
         setDarkmode(!darkmode)
@@ -17,11 +43,21 @@ export default function Inside221(props) {
             <div className="inside221Container">
                 <div className="inside221Container left-column">
                     <h1 className="inside221Container title">221 PARTY</h1>
-                    <AddFriends></AddFriends>
+                    <AddFriends toggleFriends={toggleFriends}></AddFriends>
                     <iframe className="inside221Container music-player" src="https://open.spotify.com/embed/playlist/4QFEs4By53kTqnZnRWAdbb" width="250" height="200" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
                 </div>
                 <div className="inside221Container middle-column">
                     <img className="inside221Container room-image" src={room}></img>
+                    {state.danny && <img class="inside221Container friend" src={danny} alt="danny"/>}
+                    {state.gabe && <img class="inside221Container friend" src={gabe} alt="gabe"/>}
+                    {state.john && <img class="inside221Container friend" src={john} alt="john"/>}
+                    {state.kaiden && <img class="inside221Container friend" src={kaiden} alt="kaiden"/>}
+                    {state.justin && <img class="inside221Container friend" src={justin} alt="justin"/>}
+                    {state.susan && <img class="inside221Container friend" src={susan} alt="susan"/>}
+                    {state.kayoung && <img class="inside221Container friend" src={kayoung} alt="kayoung"/>}
+                    {state.joice && <img class="inside221Container friend" src={joice} alt="joice"/>}
+                    {state.hannah && <img class="inside221Container friend" src={hannah} alt="hannah"/>}
+                    {state.annie && <img class="inside221Container friend" src={annie} alt="annie"/>}
                 </div>
                 <div className="inside221Container right-column">
                     <SoundSliders></SoundSliders>
